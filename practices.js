314ec0435009ae0c431f1ed822febbda901aba7f
batuhaandogan <tipala_1909@hotmail.com>
2018-12-26 22:58:56 -0500

    working on function practies

---
+++ practices.js
@@ -0,0 +1,17 @@
+// ES6: Use class Syntax to Define a Constructor Function
+
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
