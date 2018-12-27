4a06c1a9a4995b39605ceaf73f8f0b94773f4006
batuhaandogan <tipala_1909@hotmail.com>
2018-12-26 23:39:45 -0500

    commit

--- practices.js
+++ practices.js
@@ -15,3 +15,45 @@ class Vegetable {
 const Vegetable = makeClass();
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); // => should be 'carrot'
+
+
+
+
+
+// ES6: Use getters and setters to Control Access to an Object
+
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+// class Thermostat {
+//   constructor(Fahrenheit) {
+//   this.Fahrenheit = Fahrenheit;
+//   }
+//   get temperature() {
+//     return 5/9 * (F - 32);
+//   }
+//   set temperature(celsius) {
+//     this.Fahrenheit = celsius * 9.0 / 5 + 32; 
+//   }
+// }
+  class Thermostat{
+    constructor(farenheit){
+      this.farenheit = farenheit;
+    }
+    get temperature(){
+      return 5 / 9 * (this.farenheit - 32);
+    }
+    set temperature(celsius){
+      this.farenheit = celsius * 9.0 / 5 + 32;
+    }
+  }
+  /* Alter code above this line */
+  return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
+
